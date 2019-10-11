178babf59034c3b4eac0cadbb209b92b8c84b1f9
ValorWind1 <skyjdani@gmail.com>
2019-10-10 23:13:01 -0400

    1.1

---
+++ second-project/src/app/address-card/address-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from './user.model';
+
+@Component({
+  selector: 'app-address-card',
+  templateUrl: './address-card.component.html',
+  styleUrls: ['./address-card.component.css']
+})
+export class AddressCardComponent implements OnInit {
+
+isCollapsed: boolean = true ;
+
+  @Input('user') user: User;
+
+
+  constructor() {
+  }
+
+  toggleCollapse(){
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  ngOnInit() {
+
+  }
+
+}
---
+++ third-project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ViewModule } from './view/view.module';
+import { TestService } from './test.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ViewModule
+  ],
+  providers: [
+    TestService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
